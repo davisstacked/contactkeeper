55d098861749e2d86b105c0708a0fbfaef72e0a9
davisstacked <brettzvi@gmail.com>
2021-01-28 09:50:04 -0800

    connected MongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 
@@ -12,3 +16,4 @@ app.use('/api/contacts', require('./routes/contacts'));
 const PORT = process.env.PORT | 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
