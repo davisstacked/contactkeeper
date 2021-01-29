94683bdd0c5d4d9b017b8ff0acc47b824e7de3c3
davisstacked <brettzvi@gmail.com>
2021-01-29 14:25:05 -0800

    added hard coded contacts to ContactState

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,3 +2,40 @@ import React, { useReducer } from 'react';
 import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Jill Johnson',
+                email: 'jill@gmail.com',
+                phone: '111-111-1111',
+                type: 'personal'
+            },
+            {
+                id: 2,
+                name: 'Sara Watson',
+                email: 'sara@gmail.com',
+                phone: '222-222-2222',
+                type: 'personal'
+            },
+            {
+                id: 3,
+                name: 'Harry White',
+                email: 'harry@gmail.com',
+                phone: '333-333-3333',
+                type: 'professional'
+            }
+        ] 
+    };
+};
