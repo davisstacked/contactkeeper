3d611fc0c1d3a12263c8968e1c4fba93f01b1df1
davisstacked <brettzvi@gmail.com>
2021-02-03 13:05:07 -0800

    Add Contact

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -17,13 +17,13 @@ const Contacts = () => {
         <TransitionGroup>
             {filtered !== null 
                 ? filtered.map(contact => (
-                <CSSTransition key={contact.id} timeout={500} classNames="item">
-                    <ContactItem key={contact.id} contact={contact} />
+                <CSSTransition key={contact._id} timeout={500} classNames="item">
+                    <ContactItem contact={contact} />
                 </CSSTransition>
                 )) 
             : contacts.map(contact => (
-                <CSSTransition key={contact.id} timeout={500} classNames="item">
-                    <ContactItem key={contact.id} contact={contact} />
+                <CSSTransition key={contact._id} timeout={500} classNames="item">
+                    <ContactItem contact={contact} />
                 </CSSTransition>
             ))}
         </TransitionGroup>
