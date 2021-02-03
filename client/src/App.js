97d4702a68e864e67fb792613d6b0774f31ba1ae
davisstacked <brettzvi@gmail.com>
2021-02-03 11:40:42 -0800

    Load User and Set Token

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,17 @@ import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import Alerts from './components/layout/Alerts';
+import Alerts from './components/layout/Alerts'; 
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import AuthContext from './authContext';
 import axios from 'axios';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -25,9 +26,22 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState);
 
     // Load User
-    const loadUser = () => {
-        console.log('load user');
-    }
+    const loadUser = async () => {
+        if(localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+
+        try {
+            const res = await axios.get('/api/auth');
+
+            dispatch({
+                type: USER_LOADED, 
+                payload: res.data 
+            });
+        } catch (err) {
+            dispatch({ type: AUTH_ERROR });
+        }
+    };
 
     // Register User
     const register = async formData => {
@@ -44,6 +58,8 @@ const AuthState = props => {
                 type:REGISTER_SUCCESS,
                 payload: res.data
             });
+
+            loadUser();
         } catch (err) {
             dispatch({
                 type: REGISTER_FAIL,
