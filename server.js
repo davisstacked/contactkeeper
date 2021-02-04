c08bc86b87d6d0e749ea1cec93302c408adb07ca
davisstacked <brettzvi@gmail.com>
2021-02-04 12:27:38 -0800

    believe I found why site wont load on stack overflow. let's try deploying again.

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT | 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
